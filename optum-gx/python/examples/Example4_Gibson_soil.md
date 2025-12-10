8aa04086598cedb7faf24c8b58f3028baf7698f5
tt-oce <tt@optumce.com>
2025-12-10 11:27:02 +0100

    python example 4

---
+++ optum-gx/python/examples/Example4_Gibson_soil.md
@@ -0,0 +1,16 @@
+# 4. Strip Load on Gibson Soil
+
+The following example concerns the problem of a strip load on a semi-infinite domain of a Gibson soil, i.e., an elastic material with a Young’s modulus that increases linearly with depth from a value of zero at the top surface.
+
+## Results
+![Strip load on Gibson soil: vertical deformation field](../../static/Gibson_result_uy.png)
+
+**Figure 4.1: Strip load on Gibson soil: vertical deformation field.**
+
+## File
+
+<!-- [!file](../../static/gx-files/Example4_Gibson_soil.gxx) -->
+
+## Code
+
+:::code source="../../static/code-files/Example4.py" :::
---
+++ optum-gx/static/code-files/Example4.py
@@ -0,0 +1,70 @@
+"""Initialization"""
+from OptumGX import *
+import numpy as np
+import os
+# Application
+gx = GX()
+# Project
+prj = gx.create_project('Gibson soil')
+# Model (2D)
+model2d = prj.create_model('2D model',model_type='plane_strain')
+# Stage
+stage1 = model2d.create_stage('stage 1')
+
+"""Constants"""
+depth = 35
+nu = 0.5 # [-]
+q = 10 #kN/m^2
+#Soil profile with Gibson
+E = Profile([[0,0],[-depth,depth*0.3]]) #MPa
+"""Geometry"""
+model2d.add_rectangle(p0=[0,0],p1=[depth,-depth])
+model2d.add_line(p0=[0,0],p1=[1,0])
+
+"""Materials"""
+#Soil domain
+LinearElasticMaterial = prj.LinearElastic(name='LinearElasticMaterial',
+                            color=rgb(0,56,122),
+                            E=E,
+                            nu=nu,                        
+                            )
+#Selecting soil domain
+SoilFace = model2d.select(p0=[1,-1],types='face')
+#Setting 
+model2d.set_solid(shapes=SoilFace,material=LinearElasticMaterial)
+
+"""Loads"""
+#Surcharge
+model2d.set_surface_load(shapes=model2d.select(p0=[0.01,0],types='edge'),
+                         value=-q,
+                         direction='y',
+                         coordinate_type= 'local',
+                         option='fixed')
+
+"""Supports"""
+#Set standard supports
+model2d.set_standard_fixities()
+
+"""Analysis"""
+#Define analysis parameters
+stage1.set_analysis_properties(
+                analysis_type='deformation',
+                element_type='mixed',
+                no_of_elements=10000,
+                )
+#Begin analysis
+prj.run_analysis()
+
+"""Output"""
+uy = [stage1.output.critical_results.u_solid_y_min,stage1.output.critical_results.u_solid_y_max]
+uy = [x if x>=0 else -x for x in uy]
+uy_max = max(uy)
+print("Max vertical displacement:", round(uy_max,ndigits=5),"m")
+print(f"Deviation from theoretical: {round((uy_max-0.05)/0.05*100,2)}%")
+#Zoom and center model
+model2d.zoom_all()
+
+#Save GX file to current working directory 
+current_path = os.getcwd()
+filename ="Example4_Gibson_soil.gxx"
+gx.save_project(file_path=os.path.join(current_path, filename))
